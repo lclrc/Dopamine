e66f238dd6dfdb5431e567bb0af1baf26d9fa7af
lclrc <lclrc.3@gmail.com>
2025-03-10 14:09:31 +0800

    update link

--- Application/Dopamine/UI/DOUIManager.m
+++ Application/Dopamine/UI/DOUIManager.m
@@ -68,7 +68,7 @@
     static dispatch_once_t onceToken;
     static NSArray *releases;
     dispatch_once(&onceToken, ^{
-        NSURL *url = [NSURL URLWithString:@"https://api.github.com/repos/opa334/Dopamine/releases"];
+        NSURL *url = [NSURL URLWithString:@"https://api.github.com/repos/lclrc/Dopamine/releases"];
         NSData *data = [NSData dataWithContentsOfURL:url];
         if (data) {
             NSError *error;
--- Application/Dopamine/UI/Update/DOUpdateViewController.m
+++ Application/Dopamine/UI/Update/DOUpdateViewController.m
@@ -119,7 +119,7 @@
                     return;
                 }
             }
-            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/opa334/Dopamine/releases"] options:@{} completionHandler:nil];
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/lclrc/Dopamine/releases"] options:@{} completionHandler:nil];
             return;
         }
 
